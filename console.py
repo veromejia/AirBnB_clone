efaad4cf663323db321bffb58a2882af74cc02ae
rajsudeep <sudrocks@gmail.com>
2019-11-08 19:24:45 +0000

    Add task 6

--- console.py
+++ console.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+""" Console interpreter for the AirBnB clone """
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ commands for the interpreter to execute """
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """ exits the program """
+        raise SystemExit
+
+    def help_quit(self):
+        """ quit usage """
+        print("Quit command to exit the program\n")
+
+    def do_EOF(self, line):
+        """ exits the program """
+        return True
+
+    def help_EOF(self):
+        """ EOF usage """
+        print("EOF command (Ctrl-D) to exit the program\n")
+
+    def emptyline(self):
+        """ executes nothing when entered """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
