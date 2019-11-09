c19f18bc54f14249963a7f599c53ba88a9b66063
veromejia <veromejia.q@gmail.com>
2019-11-09 01:04:13 -0800

    linking BaseModel to FileStorage by instructions in task 5

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,13 @@
 
 
 import uuid
+import models
 from datetime import datetime
 
 
 class BaseModel:
-    """defines all common attributes/methods for other classes in the project"""
+    """defines all common attributes/methods
+    for other classes in the project"""
 
     def __init__(self, *args, **kwargs):
         """inicializating BaseModel instance"""
@@ -15,17 +17,19 @@ class BaseModel:
         if kwargs and len(kwargs) > 0:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime\
-                    (kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.\
+                        strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime\
-                    (kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"] = datetime.\
+                        strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return string representation of BaseModel class"""
@@ -37,6 +41,7 @@ class BaseModel:
         """Update the updated_at attribute with new"""
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of BaseModel class"""
