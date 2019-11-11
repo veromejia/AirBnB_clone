24c09ef155a014deb94d019db01b3c7a6341e5a7
rajsudeep <sudrocks@gmail.com>
2019-11-11 15:37:01 +0000

    Implement update method

--- console.py
+++ console.py
@@ -50,6 +50,7 @@ class HBNBCommand(cmd.Cmd):
         print("Prints the string of an instance based on the class and id\n")
 
     def do_destroy(self, line):
+        """ deletes an instances based on class and id """
         if not line:
             print("** class name missing **")
             return
@@ -73,6 +74,7 @@ class HBNBCommand(cmd.Cmd):
         print("Deletes an instance based on the class name and id\n")
 
     def do_all(self, line):
+        """ print all string representation of instances """
         if line != "":
             args = line.split(" ")
             if args[0] not in HBNBCommand.classes:
@@ -88,6 +90,39 @@ class HBNBCommand(cmd.Cmd):
     def help_all(self):
         """ all usage """
         print("Prints all str instances based or not on the class name\n")
+
+    def do_update(self, line):
+        """ updates an instance based on class and id """
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split(" ")
+        if args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        objs = storage.all()
+        key = args[0] + "." + args[1]
+        if key not in objs:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+        if len(args) < 4:
+            print("** value missing **")
+        val = objs[key]
+        try:
+            attr = getattr(val, args[2])
+            setattr(val, args[2], type(attr)(args[3]))
+        except:
+            setattr(val, args[2], args[3])
+        storage.save()
+
+    def help_update(self):
+        """ update usage """
+        print("update <class name> <id> <attribute name> <attribute value>\n")
 
     def do_quit(self, line):
         """ exits the program using quit """
