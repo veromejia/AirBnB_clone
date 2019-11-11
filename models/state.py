9faf24d681686cfa5bcd82d545889225a5ed858c
veromejia <veromejia.q@gmail.com>
2019-11-11 10:48:29 -0800

    Stte class

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""State Module for AirbnB clone"""
+
+
+from models.base_model import BaseModel
+
+
+class State:
+    """State class inherits from BaseModel"""
+
+    name = ""
