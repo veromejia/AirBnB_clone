2094a58ea5f69f3e7e31f757ec86d8b2f10e8b38
rajsudeep <sudrocks@gmail.com>
2019-11-12 20:47:41 +0000

    Add descriptions for user

--- models/user.py
+++ models/user.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """user Module for AirbnB clone"""
 
-
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User inherits from BaseModel"""
+    """
+    User inherits from BaseModel
 
+    Public Attributes:
+    (str) email: empty
+    (str) password: empty
+    (str) first_name: empty
+    (str) last_name: empty
+    """
     email = ""
     password = ""
     first_name = ""
