d2dddf8db097589f8eecd95bf6777d8a9e955cd6
rajsudeep <sudrocks@gmail.com>
2019-11-12 07:40:32 +0000

    Change name of class suite

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,7 +7,7 @@ from models.base_model import BaseModel
 import json
 import os
 
-class TestBaseModel(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
 
     """Test Cases for FileStorage Class"""
 
