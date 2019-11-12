e43ff8a53a33d297042d0b9d96e37481c606f072
rajsudeep <sudrocks@gmail.com>
2019-11-12 20:49:05 +0000

    Revert documentation

--- models/user.py
+++ models/user.py
@@ -5,15 +5,8 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    User inherits from BaseModel
+    """User inherits from BaseModel"""
 
-    Public Attributes:
-    (str) email: empty
-    (str) password: empty
-    (str) first_name: empty
-    (str) last_name: empty
-    """
     email = ""
     password = ""
     first_name = ""
