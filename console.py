03f496055574b3e67e729f6b025b647c0695efc8
rajsudeep <sudrocks@gmail.com>
2019-11-12 05:44:25 +0000

    Add new classes to console

--- console.py
+++ console.py
@@ -4,11 +4,18 @@
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """ commands for the interpreter to execute """
     prompt = "(hbnb) "
-    classes = ["BaseModel"]
+    classes = ["BaseModel", "User", "Place", "State",
+               "City", "Amenity", "Review"]
 
     def do_create(self, line):
         """ creates a new instance of base model """
