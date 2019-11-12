72be366981ef6afe1da5d16c89027ed1c40e64b8
rajsudeep <sudrocks@gmail.com>
2019-11-12 20:13:46 +0000

    Add test cases for state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Unittest for Place class"""
+
+import unittest
+from models.state import State
+import os
+
+
+class TestUser(unittest.TestCase):
+
+    """Test Cases for Place Class"""
+
+    def setUp(self):
+        """Imports module"""
+        pass
+
+    # ---------------task 8 ----------------
+    def test_init(self):
+        my_state = State()
+        self.assertTrue(isinstance(my_state, State))
+
+    def test_sub_class(self):
+        my_state = State()
+        self.assertTrue(issubclass(my_state.__class__, State))
+
+    def test_inheritance(self):
+        my_state = State()
+        self.assertTrue(hasattr(my_state, "name"))
+
+if __name__ == "__main__":
+    unittest.main()
