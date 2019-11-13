5100b3d4dd1b4e602096359f0dc09890f63aab28
rajsudeep <sudrocks@gmail.com>
2019-11-13 07:36:26 +0000

    Test dummy stuff again

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,18 +2,13 @@
 """Unittest for BaseModel class"""
 
 import unittest
-import uuid
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from models import storage
-import time
-from datetime import datetime, date, time
-from time import sleep
-import models
-import os
 
 
 class TestBaseModel(unittest.TestCase):
     """Test Cases for BaseModel Class"""
 
-    # ---------------task 3 ----------------
+    def create(self):
+        """ banana """
+        new = BaseModel()
+        new.name = "test"
