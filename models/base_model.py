45bca895080c1fc53a41b16142a6117c5309abcb
rajsudeep <sudrocks@gmail.com>
2019-11-13 08:19:53 +0000

    Test version

--- models/base_model.py
+++ models/base_model.py
@@ -12,15 +12,15 @@ class BaseModel:
     for other classes in the project"""
 
     def __init__(self, *args, **kwargs):
-        """inicializating BaseModel instance"""
+        """intiialize"""
 
-        if kwargs and len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
-                elif key != '__class__':
-                    setattr(self, key, value)
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    continue
+                if k in ['created_at', 'updated_at']:
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                self.__setattr__(k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -28,21 +28,21 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        """Return string representation of BaseModel class"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+        """string representation"""
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
-        """Update the updated_at attribute with new"""
-
+        """update updated_at"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Return a dictionary containing keys/values of __dict__."""
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = self.__class__.__name__
-        my_dict['created_at'] = self.created_at.isoformat()
-        my_dict['updated_at'] = self.updated_at.isoformat()
-        return my_dict
+        """ return dictionary representation"""
+
+        original_dict = self.__dict__.copy()
+        original_dict['__class__'] = self.__class__.__name__
+        original_dict['updated_at'] = self.updated_at.isoformat()
+        original_dict['created_at'] = self.created_at.isoformat()
+
+        return original_dict
