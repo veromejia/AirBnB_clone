86cc5c026d5794b97201b2076a885f10339fcdab
rajsudeep <sudrocks@gmail.com>
2019-11-13 05:39:37 +0000

    Revert tests in user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,15 +5,48 @@ Unittest for User class
 
 import unittest
 from models.user import User
+import os
 
 
 class TestUser(unittest.TestCase):
 
-    """
-    Test Cases for User Class
-    """
+    """Test Cases for User Class"""
+    def setUp(self):
+        """Imports module"""
+        pass
 
-    def check_name(self):
-        """ banana """
+    # ---------------task 8 ----------------
+
+    def test_init(self):
         my_user = User()
-        my_model.last_name = "Banana"
+        self.assertTrue(isinstance(my_user, User))
+
+    def test_sub_class(self):
+        my_user = User()
+        self.assertTrue(issubclass(my_user.__class__, User))
+
+    def test_inheritance(self):
+        my_user = User()
+        self.assertTrue(hasattr(my_user, "created_at"))
+        self.assertTrue(hasattr(my_user, "updated_at"))
+        self.assertTrue(hasattr(my_user, "id"))
+
+    def test_none(self):
+        my_user = User()
+        self.assertIsNotNone(my_user.id)
+        self.assertIsNotNone(my_user.created_at)
+        self.assertIsNotNone(my_user.updated_at)
+
+    def test_attr(self):
+        my_user = User()
+        self.assertTrue(hasattr(my_user, "email"))
+        self.assertTrue(hasattr(my_user, "password"))
+        self.assertTrue(hasattr(my_user, "first_name"))
+        self.assertTrue(hasattr(my_user, "last_name"))
+
+    def test_dict(self):
+        my_user = User()
+        self.assertTrue("to_dict" in dir(my_user))
+
+if __name__ == "__main__":
+    unittest.main()
