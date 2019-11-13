482127100721be40981ab1878ddf93a8ed5b6bde
rajsudeep <sudrocks@gmail.com>
2019-11-13 09:13:33 +0000

    Add some tests woo

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,108 @@
-#!/usr/bin/python3
 """Unittest for BaseModel class"""
 
 import unittest
 from models.base_model import BaseModel
+import time
+from datetime import datetime, date, time
+from time import sleep
+import models
+import os
 
 
-class TestBaseModel(unittest.Unittest):
+class TestBaseModel(unittest.TestCase):
     """Test Cases for BaseModel Class"""
-    pass
+
+    def test_id_uuid(self):
+        """ checks the type of id is string uuid"""
+
+        my_model = BaseModel()
+        self.assertEqual(str(type(my_model)),
+                         "<class 'models.base_model.BaseModel'>")
+        self.assertTrue(issubclass(type(my_model), BaseModel))
+
+    def test_id_args(self):
+        """check id is not none, and type string"""
+
+        my_model = BaseModel()
+        self.assertNotEqual(len(my_model.id), 0)
+        self.assertEqual(len(my_model.id), 36)
+        self.assertEqual(type(my_model.id), str)
+
+    def test_create_at_str(self):
+        """ check is if a str"""
+
+        my_model = BaseModel()
+        temp = my_model.to_dict()['created_at']
+        self.assertEqual(type(temp), str)
+
+    def test_create_at_datetime(self):
+        """ test datetime in create attribute"""
+
+        my_model = BaseModel()
+        my_dict = my_model.to_dict()
+        new_model = BaseModel(my_dict)
+        self.assertTrue(isinstance(new_model.created_at, datetime))
+
+    def test_str(self):
+        """ test str attribute"""
+
+        my_model = BaseModel()
+        a = "[{}] ({}) {}".\
+            format(my_model.__class__.__name__, my_model.id, my_model.__dict__)
+        self.assertEqual(str(my_model), a)
+
+    def test_save(self):
+        """ testing save method"""
+
+        my_model = BaseModel()
+        new_time = datetime.now()
+        my_model.save()
+        self.assertNotEqual(my_model.updated_at, new_time)
+
+    def test_class(self):
+        """testing BaseModel"""
+
+        my_model = BaseModel()
+        self.assertEqual(my_model.__class__.__name__, 'BaseModel')
+
+    def test_dict_created(self):
+        """testing dictionary created"""
+
+        my_model = BaseModel()
+        self.assertTrue("created_at" in my_model.to_dict())
+
+    def test_dict_updated(self):
+        """ tesying updated method"""
+
+        my_model = BaseModel()
+        self.assertTrue("updated_at" in my_model.to_dict())
+
+    def test_to_dict_attr(self):
+        """ created_at, updated_at values """
+
+        b = BaseModel()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        d = b.to_dict()
+        self.assertEqual(d["created_at"], b.created_at.strftime(time_format))
+        self.assertEqual(d["updated_at"], b.updated_at.strftime(time_format))
+        self.assertEqual(d["__class__"], "BaseModel")
+        self.assertEqual(type(d["created_at"]), str)
+        self.assertEqual(type(d["updated_at"]), str)
+
+    # -----------task 4 --------------------
+    def test_init(self):
+        """testing init method"""
+
+        my_model = BaseModel()
+        self.assertTrue(isinstance(my_model, BaseModel))
+
+    def test_init_id(self):
+        """testing init id"""
+
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+        new_model = BaseModel(**my_model_dict)
+        self.assertEqual(new_model.id, my_model.id)
+
+if __name__ == "__main__":
+    unittest.main()
