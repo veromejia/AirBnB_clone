1eb0b500c51af96540419f56de1de6530851f620
veromejia <veromejia.q@gmail.com>
2019-11-13 18:57:55 -0800

    adding comments to city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -13,6 +13,7 @@ from models import engine
 from models.engine import file_storage
 from models.engine.file_storage import FileStorage
 
+# ---------------- task 9 -------------------
 
 class TestCity(unittest.TestCase):
     """Test Cases For City Class"""
